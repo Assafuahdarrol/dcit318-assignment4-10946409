77bf5b4bf32cfd863d9cea464da9b30c180d1a30
Assafuahdarrol <dpkassafuah@st.ug.edu.gh>
2024-07-09 14:21:20 +0000

    Commit Login

---
+++ Login/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string username = usernameTxt.Text;
+            string password = passwordTxt.Text;
+            MessageBox.Show($"Username: {username}, Password: {password}");
+        }
+
+        private void button1_click(object sender, EventArgs e)
+        {
+            string username = usernameTxt.Text;
+            string password = passwordTxt.Text;
+            MessageBox.Show($"Username: {username}, Password: {password}");
+        }
+    }
+}
